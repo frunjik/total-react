6c9ee41dbee703e64af491035baacb5271383e7f
frunjik <frunjik@gmail.com>
2020-06-20 12:56:57 +0200

    ignore debug files

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import './App.css';
 import './tachyons-indigo.css';
-
 import { TextEdit } from './TextEdit';
 
 class App extends React.Component {
-	constructor(props) {
-    super(props);
-    this.state = {
-      filename: 'server/debug.js',
-      filedata: ''
-    }
-  }
 
-  render() {
+    state = {filename: '', filedata: ''}
 
+    render() {
       return (
         <div className="App">
-          <TextEdit height="332px" filename="projects/total-react/client/src/App.js"></TextEdit>
-          <TextEdit height="470px" filename="projects/urbit/smith/urbit/app/smith.hoon"></TextEdit>
+          <TextEdit height="380px" filename="projects/total-react/client/src/App.js"></TextEdit>
+          <TextEdit height="120px" filename="projects/total-react/client/src/TextEdit.js"></TextEdit>
+          <TextEdit height="200px" filename="projects/urbit/smith/urbit/app/smith.hoon"></TextEdit>
         </div>
       );
     }
 }
-
 export default App;
